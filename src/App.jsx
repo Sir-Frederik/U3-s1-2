3c7ffe27ffe403d3cc84c70491f1ca4ea19b4a01
Federico <brunetti.federico.13@gmail.com>
2025-05-14 09:07:46 +0200

    sistemato return mancante in footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import MyNav from "./components/MyNav";
 import Footer from "./components/Footer";
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
     <>
       <MyNav brandName="Ciccio pasty"></MyNav>
+      <Welcome />
       <Footer alertColor="success" />
     </>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import { Container } from "react-bootstrap";
 
 function Footer() {
-  <footer className="bg-dark text-white">AStrubazy &copy;</footer>;
+  return <footer className="bg-dark text-white">AStrubazy &copy;</footer>;
 }
 export default Footer;
