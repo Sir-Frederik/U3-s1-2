02feb4afbe0bdbe91aab5046c1ca8fcf6af55f50
Federico <brunetti.federico.13@gmail.com>
2025-05-14 16:21:35 +0200

    create colonne delle carta e key del map

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -1,7 +1,7 @@
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import scifi from "../data/scifi.json";
-import { Container, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 
 function AllTheBooks() {
   return (
@@ -9,15 +9,17 @@ function AllTheBooks() {
       <h2> Libri Disponibili </h2>
       <Row>
         {scifi.map((book) => (
-          <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src={book.img} />
-            <Card.Body>
-              <Card.Title>{book.title}</Card.Title>
-              <Card.Text>{book.category}</Card.Text>
-              <Card.Text>{book.price}$</Card.Text>
-              <Button variant="primary">Acquista</Button>
-            </Card.Body>
-          </Card>
+          <Col key={book.asin}>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={book.img} />
+              <Card.Body>
+                <Card.Title>{book.title}</Card.Title>
+                <Card.Text>{book.category}</Card.Text>
+                <Card.Text>{book.price}$</Card.Text>
+                <Button variant="primary">Acquista</Button>
+              </Card.Body>
+            </Card>
+          </Col>
         ))}
       </Row>
     </Container>
